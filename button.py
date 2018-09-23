b00ad29bab250a7294eead140883725531d551d2
Elmer Sevillano <esevillano@ymail.com>
2018-09-22 18:04:00 -0700

    Create button class to set up play button

---
+++ button.py
@@ -0,0 +1,15 @@
+import pygame.font
+
+
+class Button:
+
+    def __init__(self, settings, screen, msg):
+        """Initialize button attributes."""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
