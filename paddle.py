9d1f3aeff0beedc43ba1b639c91b057b32e21f52
Elmer Sevillano <esevillano@ymail.com>
2018-09-25 08:16:29 -0700

    Add if-elif statement to init method to set the location for each paddle

--- paddle.py
+++ paddle.py
@@ -2,9 +2,25 @@ import pygame
 from pygame.sprite import Sprite
 
 class Paddle(Sprite):
-    def __init__(self, settings, screen):
+    def __init__(self, settings, wall):
         """Initialize the paddles and set their starting position."""
-        print('Paddle initialized')
+
+        super().__init__()
+
+        if wall == "left":
+            self.location = pygame.Rect(settings.paddleOffset, settings.verticalPosition, settings.lineThickness, settings.paddleSize)
+        elif wall == "right":
+            self.location = pygame.Rect(settings.screen_width - settings.paddleOffset - settings.lineThickness, settings.verticalPosition, settings.lineThickness, settings.paddleSize)
+        elif wall == "top":
+            self.location = pygame.Rect(settings.horizontalPosition, settings.paddleOffset, settings.paddleSize, settings.lineThickness)
+        elif wall == "bottom":
+            self.location = pygame.Rect(settings.horizontalPosition, settings.screen_height - settings.paddleOffset - settings.lineThickness, settings.paddleSize, settings.lineThickness)
+
+        # Movement flags
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_top = False
+        self.moving_down = False
 
     def update(self):
         """Update the paddle's position based on the movement flags."""
@@ -16,7 +32,3 @@ class Paddle(Sprite):
             self.center += self.settings.paddle_speed_factor
         if self.moving_up and self.rect.up > 0:
             self.center -= self.settings.paddle_speed_factor
-
-    def blitme(self):
-        """Draw the paddle at its current location."""
-        self.screen.blit(self.image, self.rect)
