7e8090b3db0de5fc94689e5a722a19b7827f4ce7
Elmer Sevillano <esevillano@ymail.com>
2018-09-27 17:44:39 -0700

    Update create_paddles to create paddles for player and ai and add if-elif to check_events methods

--- game_functions.py
+++ game_functions.py
@@ -7,40 +7,73 @@ from ball import Ball
 from paddle import Paddle
 
 
-def create_paddles(settings):
-    # Create the Group paddles
-    paddles = Group()
+def create_paddles(settings, screen, player_paddles, ai_paddles):
+    # Create the player paddles
+    playerLeftPaddle = Paddle(settings, screen, "left", "player")
+    playerTopPaddle = Paddle(settings, screen, "top", "player")
+    playerBottomPaddle = Paddle(settings, screen, "bottom", "player")
 
-    # Create all four paddles
-    leftPaddle = Paddle(settings, "left")
-    rightPaddle = Paddle(settings, "right")
-    topPaddle = Paddle(settings, "top")
-    bottomPaddle = Paddle(settings, "bottom")
+    # Create the three ai paddles
+    aiRightPaddle = Paddle(settings, screen, "right", "ai")
+    aiTopPaddle = Paddle(settings, screen, "top", "ai")
+    aiBottomPaddle = Paddle(settings, screen, "bottom", "ai")
 
-    # Add the paddles to the sprite Group
-    paddles.add(leftPaddle)
-    paddles.add(rightPaddle)
-    paddles.add(topPaddle)
-    paddles.add(bottomPaddle)
+    # Add the player paddles to the sprite Group
+    player_paddles.add(playerLeftPaddle)
+    player_paddles.add(playerTopPaddle)
+    player_paddles.add(playerBottomPaddle)
 
-    # Return the Group of paddles
-    return paddles
+    # Add the ai paddles to their own sprite Group
+    ai_paddles.add(aiRightPaddle)
+    ai_paddles.add(aiTopPaddle)
+    ai_paddles.add(aiBottomPaddle)
 
 
 def create_ball():
     print("Hello")
 
 
-def check_keydown_events():
-    print("Hello")
+def check_keydown_events(event, paddles):
+    keys = pygame.key.get_pressed()
+    for paddle in paddles:
+        if keys[pygame.K_UP] and paddle.wall == "left" and paddle.user == "player":
+            paddle.moving_up = True
+        elif keys[pygame.K_DOWN] and paddle.wall == "left" and paddle.user == "player":
+            paddle.moving_down = True
+        elif keys[pygame.K_LEFT] and not keys[pygame.K_LSHIFT] and paddle.wall == "top" and paddle.user == "player":
+            paddle.moving_left = True
+        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LSHIFT] and paddle.wall == "top" and paddle.user == "player":
+            paddle.moving_right = True
+        # https://stackoverflow.com/questions/12556535/press-multiple-keys-at-once-to-get-my-character-to-move-diagonally
+        # Used in understanding how to incorporate left shift in game
+        elif keys[pygame.K_LSHIFT] and keys[pygame.K_LEFT] and paddle.wall == "bottom" and paddle.user == "player":
+            paddle.moving_left = True
+        elif keys[pygame.K_LSHIFT] and keys[pygame.K_RIGHT] and paddle.wall == "bottom" and paddle.user == "player":
+            paddle.moving_right = True
+        elif keys[pygame.K_q]:
+            sys.exit()
 
 
-def check_keyup_events():
-    print("Hello")
+def check_keyup_events(event, paddles):
+    for paddle in paddles:
+        if event.key == pygame.K_LEFT:
+            paddle.moving_left = False
+        elif event.key == pygame.K_RIGHT:
+            paddle.moving_right = False
+        elif event.key == pygame.K_UP:
+            paddle.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            paddle.moving_down = False
 
 
-def check_events():
-    print("Hello")
+def check_events(settings, screen, paddles):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, paddles)
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, paddles)
 
 
 def check_play_button():
