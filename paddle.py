10f7523065f2b57246661f558eded56209b8f7f9
Elmer Sevillano <esevillano@ymail.com>
2018-09-27 17:42:01 -0700

    Create attributes and refactor code to set the location properly for all six paddles

--- paddle.py
+++ paddle.py
@@ -1,34 +1,67 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Paddle(Sprite):
-    def __init__(self, settings, wall):
+    def __init__(self, settings, screen, wall, user):
         """Initialize the paddles and set their starting position."""
 
         super().__init__()
 
-        if wall == "left":
-            self.location = pygame.Rect(settings.paddleOffset, settings.verticalPosition, settings.lineThickness, settings.paddleSize)
-        elif wall == "right":
-            self.location = pygame.Rect(settings.screen_width - settings.paddleOffset - settings.lineThickness, settings.verticalPosition, settings.lineThickness, settings.paddleSize)
-        elif wall == "top":
-            self.location = pygame.Rect(settings.horizontalPosition, settings.paddleOffset, settings.paddleSize, settings.lineThickness)
-        elif wall == "bottom":
-            self.location = pygame.Rect(settings.horizontalPosition, settings.screen_height - settings.paddleOffset - settings.lineThickness, settings.paddleSize, settings.lineThickness)
+        # Attributes to allow the paddle to be distinguished between player an ai
+        self.user = user
+        self.wall = wall
+
+        # Values used in assigning location of paddle
+        self.hor = settings.horizontalPosition
+        self.vert = settings.verticalPosition
+        self.wide = settings.lineThickness
+        self.long = settings.paddleSize
+        self.height = settings.screen_height
+        self.width = settings.screen_width
+        self.offset = settings.paddleOffset
+
+        # Values used when moving the paddles
+        self.settings = settings
+        self.screen_rect = screen.get_rect()
+
+        # Adjust the location of the paddle based on the position and user
+        if self.wall == "left":
+            self.location = pygame.Rect(self.offset, self.vert, self.wide, self.long)
+        elif self.wall == "right":
+            self.location = pygame.Rect(self.width - self.offset - self.wide, self.vert, self.wide, self.long)
+        elif self.wall == "top":
+            if self.user == "player":
+                self.hor -= self.width/4 + self.long/2
+                self.location = pygame.Rect(self.hor, self.offset, self.long, self.wide)
+            elif self.user == "ai":
+                self.hor += self.width/4 - self.long/2
+                self.location = pygame.Rect(self.hor, self.offset, self.long, self.wide)
+        elif self.wall == "bottom":
+            if self.user == "player":
+                self.hor -= self.width/4 + self.long/2
+                self.location = pygame.Rect(self.hor, self.height - self.offset - self.wide, self.long, self.wide)
+            elif self.user == "ai":
+                self.hor += self.width/4 - self.long/2
+                self.location = pygame.Rect(self.hor, self.height - self.offset - self.wide, self.long, self.wide)
 
         # Movement flags
         self.moving_left = False
         self.moving_right = False
-        self.moving_top = False
+        self.moving_up = False
         self.moving_down = False
 
     def update(self):
         """Update the paddle's position based on the movement flags."""
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.settings.paddle_speed_factor
-        if self.moving_left and self.rect.left > 0:
-            self.center -= self.settings.paddle_speed_factor
-        if self.moving_down and self.rect.down < self.screen_rect.down:
-            self.center += self.settings.paddle_speed_factor
-        if self.moving_up and self.rect.up > 0:
-            self.center -= self.settings.paddle_speed_factor
+        if self.moving_right and self.location.x + self.long < self.width/2:
+            self.location.x += self.settings.paddle_speed_factor
+        if self.moving_left and self.location.x > 0:
+            self.location.x -= self.settings.paddle_speed_factor
+        if self.moving_down and self.location.y + self.long < self.height:
+            self.location.y += self.settings.paddle_speed_factor
+        if self.moving_up and self.location.y > 0:
+            self.location.y -= self.settings.paddle_speed_factor
+
+    # Use to test the movement of the paddles
+    def __str__(self):
+        print("wall: " + str(self.wall) + " x: " + str(self.location.centerx) + " y: " + str(self.location.centery) + " screen width: " + str(self.width) + " screen height: " + str(self.height))
