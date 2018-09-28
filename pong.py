4c1abe1ed2a41947f631eea5a88135a2503fb20e
Elmer Sevillano <esevillano@ymail.com>
2018-09-28 12:06:03 -0700

    Refactor code to move some function calls to update_screen

--- pong.py
+++ pong.py
@@ -23,34 +23,25 @@ def run_game():
     screen = pygame.display.set_mode((pong_settings.screen_width, pong_settings.screen_height))
     pygame.display.set_caption("Pong")
 
-    # # Make the ball and paddles
-    # ball = pygame.draw.circle(screen, pong_settings.white, pong_settings.position, pong_settings.radius, pong_settings.width)
-
+    # # Make the ball and instantiate groups of paddles
+    ball = Ball(pong_settings)
     paddles = Group()
-
     player_paddles = Group()
     ai_paddles = Group()
 
+    # Create each set of paddles
     gf.create_paddles(pong_settings, screen, player_paddles, ai_paddles)
 
+    # Add the player and ai paddles to the group of paddles
     paddles.add(player_paddles)
     paddles.add(ai_paddles)
 
+    # Activate the left shift key
+    pygame.key.set_mods(pygame.KMOD_LSHIFT)
+
     while True:
-        screen.fill(pong_settings.black)
+        gf.update_screen(pong_settings, screen, ball, paddles)
         gf.check_events(pong_settings, screen, paddles)
-
-        pygame.key.set_mods(pygame.KMOD_LSHIFT)
-
-        for paddle in paddles:
-            pygame.draw.rect(screen, pong_settings.white, paddle.location)
-            paddle.update()
-
-        # ball
-
-        # pygame.draw.line(screen, pong_settings.white, (pong_settings.screen_width/2, 0), (pong_settings.screen_width/2, pong_settings.screen_height))
-
-        pygame.display.flip()
 
 
 if __name__ == "__main__":
